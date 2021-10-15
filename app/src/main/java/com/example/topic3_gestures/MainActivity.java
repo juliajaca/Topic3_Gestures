8f16160913b2ecac103dd4c9d06e6aa6b7941ae0
jjaca <jjaca@cifpfbmoll.eu>
2021-10-15 19:44:38 +0200

    https://developer.android.com/training/gestures/detector - Ejercicios de Cómo detectar un subconjunto de gestos compatibles Junto a como obtener las X y las Y de donde clicas

--- app/src/main/java/com/example/topic3_gestures/MainActivity.java
+++ app/src/main/java/com/example/topic3_gestures/MainActivity.java
@@ -1,102 +1,54 @@
 package com.example.topic3_gestures;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 import androidx.core.view.MotionEventCompat;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //parte 3
+    private GestureDetectorCompat mDetector;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Añadimos el listener al botón (el boton es una vista)
-        //https://stackoverflow.com/questions/39836303/cannot-resolve-symbol-setontouchlistener
-        View myView = findViewById(R.id.button);
-        myView.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v , MotionEvent event) {
-                //Le mando hacer algo
-                //ejemplo con el toast
-                Context context = getApplicationContext();
-                CharSequence text = "Hello toast! Evento!";
-                int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-                // ejemplo con MotionEvent
-                if (event.getAction() == MotionEvent.ACTION_DOWN ) {
-                    //Insert desired code here
-                    Log.d("movimiento", "ACCIONNNNNNNN APOYO");
-                    return true;
-                }
-                return false;
-
-            }
-        });
-
-        //pongo un listener en mi string de hola munda
-        View holaMundo = findViewById(R.id.textView);
-        holaMundo.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                //Le mando hacer algo
-                //ejemplo con el toast
-                Context context = getApplicationContext();
-                CharSequence text = "Hello toast! Evento! HOLA MUNDO";
-                int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-                // ejemplo con MotionEvent
-                if (event.getAction() == MotionEvent.ACTION_DOWN ) {
-                    //Insert desired code here
-                    Log.d("movimiento", "ACCIONNNNNNNN HOLA");
-                    return true;
-                }
-                return false;
-
-            }
-        });
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
     }
-    // This example shows an Activity, but you would use the same approach if
-    // you were subclassing a View.
+
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-
-        int action = MotionEventCompat.getActionMasked(event);
-
-        switch (action) {
-            case (MotionEvent.ACTION_DOWN):
-                Log.d("movimiento", "Action was DOWN");
-                return true;
-            case (MotionEvent.ACTION_MOVE):
-                Log.d("movimiento", "Action was MOVE");
-                Log.d("movimientoAccion", MotionEvent.actionToString(MotionEvent.ACTION_MOVE));
-                return true;
-            case (MotionEvent.ACTION_UP):
-                Log.d("movimiento", "Action was UP");
-                return true;
-            case (MotionEvent.ACTION_CANCEL):
-                Log.d("movimiento", "Action was CANCEL");
-                return true;
-            case (MotionEvent.ACTION_OUTSIDE):
-                Log.d("movimiento", "Movement occurred outside bounds " +
-                        "of current screen element");
-                return true;
-            default:
-                //capturar otras acciones
-                Log.d("movimiento", "Cosa ocurrida diferente");
-                return super.onTouchEvent(event);
-        }
-
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final String DEBUG_TAG = "Gestos";
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            Log.d(DEBUG_TAG,"onDown: " + event.toString());
+            Log.d("IdPointer", MotionEvent.actionToString(MotionEvent.ACTION_DOWN));
+            Log.d("position", MotionEvent.axisToString(0));
+            Log.d("Posicion de la X", String.valueOf(event.getX()));
+            Log.d("Posicion de la Y", String.valueOf(event.getY()));
+            return true;
+        }
+        
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+            return true;
+        }
+    }
 
 
 }
